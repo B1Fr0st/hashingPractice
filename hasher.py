7bea8cf8c0a48036c3491a4ee0b5f939a14b723e
B1Fr0st <lovewolves413@gmail.com>
2022-04-27 15:31:43 -0700

    Finish password salter

--- hasher.py
+++ hasher.py
@@ -8,6 +8,11 @@ def saltPassword(password:str,saltLength:int=64) -> str:
     saltStr = ""
     for i in range(0,saltLength):
         saltStr = saltStr+secrets.choice(string.ascii_letters)
-    return password+saltStr
+    return [password+saltStr,saltStr]
 def hashPassword(password:str) -> str:
-    """CRITICAL: PASSWORD SHOULD NOT BE SALTED, UNLESS IT IS BEING CALLED RECURSIVELY ON PURPOSE"""
+    """
+    CRITICAL: PASSWORD SHOULD NOT BE SALTED, UNLESS IT IS BEING CALLED RECURSIVELY ON PURPOSE
+    Returns a salted and hashed password, along with the salt.
+    """
+    saltedPass = saltPassword(password)
+    return [h256(saltedPass[0]),saltedPass[1]]
