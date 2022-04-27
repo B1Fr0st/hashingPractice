f0ec572da67034148ff29070dc174b4870794b39
B1Fr0st <lovewolves413@gmail.com>
2022-04-27 15:23:08 -0700

    Construct Salter

--- hasher.py
+++ hasher.py
@@ -1,1 +1,13 @@
 #hasher.py
+import hashlib,secrets,string
+
+def h256(hashStr:str) -> str:
+    """Easier way to hash a string using SHA-256"""
+    return hashlib.sha256(hashStr.encode("utf-8")).hexdigest()
+def saltPassword(password:str,saltLength:int=64) -> str:
+    saltStr = ""
+    for i in range(0,saltLength):
+        saltStr = saltStr+secrets.choice(string.ascii_letters)
+    return password+saltStr
+def hashPassword(password:str) -> str:
+    """CRITICAL: PASSWORD SHOULD NOT BE SALTED, UNLESS IT IS BEING CALLED RECURSIVELY ON PURPOSE"""
